37b852341950219e9438989358aad26bd234ac5d
rnat697 <rnat697@aucklanduni.ac.nz>
2022-05-11 10:22:43 +1200

    added seven segment on block diagram for the mouse interface

--- miniproject files/MiniprojectResources/seven_seg.vhd
+++ miniproject files/MiniprojectResources/seven_seg.vhd
@@ -1,27 +1,27 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-entity BCD2SevenSeg is
-  port (digit : in unsigned(3 downto 0);
-        LED_out : out unsigned(7 downto 0));
+entity seven_seg is
+  port (Din : in unsigned(3 downto 0);
+        Dout : out unsigned(6 downto 0));
 end entity;
 
-architecture arc2 of BCD2SevenSeg is
+architecture arc2 of seven_seg is
 begin
-  process (digit)
+  process (Din)
   begin
-    case digit is 
-      when "0000" => LED_out <= "11000000"; -- 0
-      when "0001" => LED_out <= "11111001"; -- 1 11111001
-		  when "0010" => LED_out <= "10100100"; -- 2 10100100
-      when "0011" => LED_out <= "10110000"; -- 3 10110000
-      when "0100" => LED_out <= "10011001"; -- 4 10011001
-      when "0101" => LED_out <= "10010010"; -- 510010010
-      when "0110" => LED_out <= "10000010"; -- 6 10000010
-      when "0111" => LED_out <= "11111000"; -- 7 11111000
-      when "1000" => LED_out <= "10000000"; -- 8 10000000
-      when "1001" => LED_out <= "10010000"; -- 9 10010000
-      when others => LED_out <= "11111111"; 
+    case Din is 
+      when "0000" => Dout <= "1000000"; -- 0
+      when "0001" => Dout <= "1111001"; -- 1 11111001
+		  when "0010" => Dout <= "0100100"; -- 2 10100100
+      when "0011" => Dout <= "0110000"; -- 3 10110000
+      when "0100" => Dout <= "0011001"; -- 4 10011001
+      when "0101" => Dout <= "0010010"; -- 510010010
+      when "0110" => Dout <= "0000010"; -- 6 10000010
+      when "0111" => Dout <= "1111000"; -- 7 11111000
+      when "1000" => Dout <= "0000000"; -- 8 10000000
+      when "1001" => Dout <= "0010000"; -- 9 10010000
+      when others => Dout <= "1111111"; 
     end case;
 
   end process; 
