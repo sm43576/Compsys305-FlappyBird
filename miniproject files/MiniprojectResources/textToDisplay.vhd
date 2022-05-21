d8a35b19616714ed9119bdff98651ebcb07a0f2f
rnat697 <rnat697@aucklanduni.ac.nz>
2022-05-21 18:02:19 +1200

    Added texts for instructions on how to start the game
    - charrom values need to be changed for train title and norm title

--- miniproject files/MiniprojectResources/textToDisplay.vhd
+++ miniproject files/MiniprojectResources/textToDisplay.vhd
@@ -15,8 +15,23 @@ architecture a OF textToDisplay IS
 	signal p_row: integer;
 	signal p_col: integer;
 	signal value: std_logic_vector(5 downto 0);
-	type dummy_array is array(0 to 11) of std_logic_vector(5 downto 0);
-   signal ins_dummy : dummy_array := ("000110","001100","000001","010000","010000","011001","100000", "000010", "001001", "010010","000100","100000");
+	type title is array(0 to 11) of std_logic_vector(5 downto 0);
+   signal sig_title : title := ("000110","001100","000001","010000",
+											"010000","011001","100000", "000010", 
+											"001001", "010010","000100","100000"); -- FLAPPY BIRD
+	type trainTitle is array(0 to 24) of std_logic_vector(5 downto 0);
+   signal sig_trainT : trainTitle := ("011000","010110","000001","001011","010000",
+													"100000","001111","010001","000100","000101",
+													"100000","010100","010110","000101","010111",
+													"010111", "100000", "000010","011001","011000",
+													"011000","010001","010000", "100000","111100"); -- TRAIN MODE PRESS BUTTON 0
+	type normTitle is array(0 to 23) of std_logic_vector(5 downto 0);
+   signal sig_normT : normTitle := ("000111","000001","001111","000101", "100000", 
+												"001111","010001","000100","000101", "100000",
+												"010100","010110","000101","010111","010111", 
+												"100000", "000010","011001","011000","011000",
+												"010001","010000", "100000","111101"); -- GAME MODE PRESS BUTTON 1
+	
 
 	
 	
@@ -27,19 +42,37 @@ begin
 	p_col <= conv_integer(unsigned(pix_col));
 	--if(mode = '000') then
 	
-	--title[]= [];
+	-- Flappy Bird Title
 	for i in 0 to 11 loop
 	
-	
-	if(160<p_row and p_row<192) and (((i-1)*32)+96<p_col and p_col<96+(i*32))	 then
-	   value <= ins_dummy(i);
-		character_address <= value ; -- L
-		font_row <= pix_row(4 downto 2);
-		font_col <= pix_col(4 downto 2);
-	end if;
+		if(96<p_row and p_row<128) and (((i-1)*32)+96<p_col and p_col<96+(i*32))	 then
+			value <= sig_title(i);
+			character_address <= value ; -- L
+			font_row <= pix_row(4 downto 2);
+			font_col <= pix_col(4 downto 2);
+		end if;
 	end loop;
 	
+	-- train start instructions
+	for i in 0 to 24 loop
 	
+		if(320<p_row and p_row<336) and (((i-1)*16)+96<p_col and p_col<96+(i*16))	 then
+			value <= sig_trainT(i);
+			character_address <= value ; 
+			font_row <= pix_row(3 downto 1);
+			font_col <= pix_col(3 downto 1);
+		end if;
+	end loop;
+	
+	-- Normal game mode start instruction
+	for i in 0 to 23 loop
+		if(336<p_row and p_row<352) and (((i-1)*16)+96<p_col and p_col<96+(i*16))	 then
+			value <= sig_normT(i);
+			character_address <= value ; 
+			font_row <= pix_row(3 downto 1);
+			font_col <= pix_col(3 downto 1);
+		end if;
+	end loop;
 	--elsif (mode = '001') then
 	-- score
 	
