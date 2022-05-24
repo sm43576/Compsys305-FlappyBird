9e17d4f179dbb58bb783db9881942934a2961493
sm43576 <79734318+sm43576@users.noreply.github.com>
2022-05-24 18:47:07 +1200

    short commit

--- miniproject files/MiniprojectResources/pipes.vhd
+++ miniproject files/MiniprojectResources/pipes.vhd
@@ -28,8 +28,8 @@ signal x2:integer;
 
 BEGIN     
 --dimension 600x479
-x1<= 0;
-x2<= 360;
+x1<= 50;
+x2<= 365;
 sizex <= CONV_STD_LOGIC_VECTOR(20,10); -- 20 pixels in width
 sizey <= CONV_STD_LOGIC_VECTOR(100,10); -- height may change depending on random numeber
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of pipe
@@ -43,7 +43,7 @@ temp_pipe1_on <= '1' when ( ('0' & pipe_x_pos <= '0' & pixel_column + sizex) and
 					and ('0' & pipe1_y_pos <= pixel_row + sizey) and ('0' & pixel_row <= pipe1_y_pos + sizey) and (mode = "001" or mode = "010") )  else	-- y_pos - size <= pixel_row <= y_pos + size
 			'0';
 temp_pipe2_on <= '1' when ( ('0' & pipe_x_pos <= '0' & pixel_column + sizex) and ('0' & pixel_column <= '0' & pipe_x_pos + sizex) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & pipe2_y_pos <= pixel_row + sizey) and ('0' & pixel_row <= 479) and (mode = "001" or mode = "010") )  else	-- y_pos - size <= pixel_row <= y_pos + size
+					and ('0' & pipe2_y_pos <= pixel_row + sizey) and ('0' & pixel_row <= pipe2_y_pos + sizey) and (mode = "001" or mode = "010") )  else	-- y_pos - size <= pixel_row <= y_pos + size
 			'0';
 --checks if pixel is in the ball boundary 
 pipe_on <= temp_pipe1_on or temp_pipe2_on;
