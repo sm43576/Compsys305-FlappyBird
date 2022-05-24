942ea23f4871a2e1e5e3ce91f77184cafac115a7
rnat697 <rnat697@aucklanduni.ac.nz>
2022-05-24 17:49:49 +1200

    Added showing pipes to bdf file

--- miniproject files/MiniprojectResources/pipes.vhd
+++ miniproject files/MiniprojectResources/pipes.vhd
@@ -14,25 +14,33 @@ END pipes;
 
 architecture behavior of pipes is
 
-SIGNAL temp_ball_on					: std_logic;
-SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
-SIGNAL top_pipe_y_pos				: std_logic_vector(9 DOWNTO 0);
-SiGNAL pipe_x_pos				: std_logic_vector(10 DOWNTO 0); --change to match mouse movement.
+SIGNAL temp_pipe1_on			: std_logic;
+signal temp_pipe2_on			: std_logic;
+SIGNAL sizex 					: std_logic_vector(9 DOWNTO 0);
+signal sizey 					: std_logic_vector(9 downto 0);  
+SIGNAL pipe1_y_pos			: std_logic_vector(9 DOWNTO 0);
+signal pipe2_y_pos			: std_logic_vector(9 downto 0);
+SiGNAL pipe_x_pos				: std_logic_vector(10 DOWNTO 0);
 SIGNAL pipe_x_motion			: std_logic_vector(9 DOWNTO 0);
 
 
-BEGIN        
-size <= CONV_STD_LOGIC_VECTOR(8,10);
--- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
+BEGIN     
+sizex <= CONV_STD_LOGIC_VECTOR(20,10); -- 20 pixels in width
+sizey <= CONV_STD_LOGIC_VECTOR(100,10); -- height may change depending on random numeber
+-- ball_x_pos and ball_y_pos show the (x,y) for the centre of pipe
 
-ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,11);
---ball_y_pos <= CONV_STD_LOGIC_VECTOR(10,10);
+pipe_x_pos <= CONV_STD_LOGIC_VECTOR(600,11);
+pipe1_y_pos <= CONV_STD_LOGIC_VECTOR(100,10);
+pipe2_y_pos <= CONV_STD_LOGIC_VECTOR(360,10);
 
 						-- and here is adding 0 to ball_x_pos making it unsigned
-temp_ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+temp_pipe1_on <= '1' when ( ('0' & pipe_x_pos <= '0' & pixel_column + sizex) and ('0' & pixel_column <= '0' & pipe_x_pos + sizex) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & pipe1_y_pos <= pixel_row + sizey) and ('0' & pixel_row <= pipe1_y_pos + sizey) and (mode = "001" or mode = "010") )  else	-- y_pos - size <= pixel_row <= y_pos + size
+			'0';
+temp_pipe2_on <= '1' when ( ('0' & pipe_x_pos <= '0' & pixel_column + sizex) and ('0' & pixel_column <= '0' & pipe_x_pos + sizex) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & pipe2_y_pos <= pixel_row + sizey) and ('0' & pixel_row <= pipe2_y_pos + sizey) and (mode = "001" or mode = "010") )  else	-- y_pos - size <= pixel_row <= y_pos + size
 			'0';
 --checks if pixel is in the ball boundary 
-ball_on <= temp_ball_on;
+pipe_on <= temp_pipe1_on and temp_pipe2_on;
 
 end behavior;   
