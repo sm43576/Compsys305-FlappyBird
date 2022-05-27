4f0639c43be83aa16bdd8e51d66bb5aa0370efa3
sm43576 <79734318+sm43576@users.noreply.github.com>
2022-05-27 16:56:34 +1200

    Revert "Revert "testbackground""
    This reverts commit e5df65db9c9eade2a353582db1f5c78b9e7c04b4.

--- miniproject files/MiniprojectResources/background.vhd
+++ miniproject files/MiniprojectResources/background.vhd
@@ -6,7 +6,8 @@ use  IEEE.STD_LOGIC_UNSIGNED.all;
 ENTITY Background IS
 	PORT(	clock_25Mhz: IN	STD_LOGIC;
 			mode: IN STD_LOGIC_VECTOR (2 downto 0); --- main menu, training mode, game mode, game over, last difficulty background
-			red_out, green_out, blue_out, backgroundOn	: OUT	STD_LOGIC);
+			backgroundOn: OUT std_logic;
+			red_out, green_out, blue_out	: OUT	STD_LOGIC_VECTOR(3 downto 0));
 end Background;
 
 architecture a OF Background IS
@@ -18,38 +19,37 @@ begin
 backgroundOn <= '1';
 	if(mode = "000") then
 	-- Main menu colour (white)
-		red_out <= '1';
-		green_out <= '1';
-		blue_out <= '1';
+		red_out <= "1111";
+		green_out <= "1111";
+		blue_out <= "1111";
 	
 	elsif(mode = "001") then
 	--- Training mode
 		
-		red_out <= '0';
-		green_out <= '1';
-		blue_out <= '1';
+		red_out <= "0000";
+		green_out <= "1111";
+		blue_out <= "1111";
 
 	elsif(mode = "010") then
 	--- Game mode
-		red_out <= '1';
-		green_out <= '1';
-		blue_out <= '1';
-	
+		red_out <= "1111";
+		green_out <= "0000";
+		blue_out <= "1111";
 		
 	elsif(mode = "011" or mode = "100") then
 	--- Game Over
 
-		red_out <= '0';
-		green_out <= '0';
-		blue_out <= '0';
+		red_out <= "0000";
+		green_out <= "0000";
+		blue_out <= "0000";
 
 		
 	elsif(mode = "101") then
 	--- Last difficulty in game mode
 		
-		red_out <= '1';
-		green_out <= '1';
-		blue_out <= '0';
+		red_out <= "1111";
+		green_out <= "1111";
+		blue_out <= "0000";
 	end if;
 	end process modeColours;
 end a;
