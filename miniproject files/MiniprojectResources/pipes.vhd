77a16403a427decad252b1c942b76306886a01f0
sm43576 <79734318+sm43576@users.noreply.github.com>
2022-05-27 17:39:54 +1200

    finish renaming

--- miniproject files/MiniprojectResources/pipes.vhd
+++ miniproject files/MiniprojectResources/pipes.vhd
@@ -15,14 +15,18 @@ END pipes;
 
 architecture behavior of pipes is
 
-SIGNAL temp_pipe1_on			: std_logic;
-signal temp_pipe2_on			: std_logic;
-SIGNAL sizex 					: std_logic_vector(9 DOWNTO 0);
-signal sizey 					: std_logic_vector(9 downto 0);  
-SIGNAL pipe1_y_pos			: std_logic_vector(9 DOWNTO 0);
-signal pipe2_y_pos			: std_logic_vector(9 downto 0);
-SiGNAL pipe_x_pos				: std_logic_vector(10 DOWNTO 0);
-SIGNAL pipe_x_motion			: std_logic_vector(10 DOWNTO 0);
+SIGNAL temp_pipetop1_on			: std_logic;
+signal temp_pipebottom1_on			: std_logic;
+
+SIGNAL temp_pipetop2_on			: std_logic;--pipenumber2
+signal temp_pipebottom2_on			: std_logic;--pipenumber2
+
+SIGNAL sizex 					: std_logic_vector(9 DOWNTO 0);-- both pipes
+signal sizey 					: std_logic_vector(9 downto 0);--bothpipes
+SIGNAL pipe1top_y_pos			: std_logic_vector(9 DOWNTO 0);
+signal pipe1bottom_y_pos			: std_logic_vector(9 downto 0);
+SiGNAL pipe1_x_pos				: std_logic_vector(10 DOWNTO 0);
+SIGNAL pipe1_x_motion			: std_logic_vector(10 DOWNTO 0);
 signal newLength :std_logic_vector(9 downto 0);
 signal gap : std_logic_vector(9 downto 0):= "0001100100"; -- 50 pixel gap
 signal reset: std_logic:='1';
@@ -36,21 +40,22 @@ BEGIN
 		
 		sizex <= CONV_STD_LOGIC_VECTOR(20,10); -- 20 pixels in width
 		sizey <= newLength; -- height may change depending on random numeber
+		
 		-- ball_x_pos and ball_y_pos show the (x,y) for the centre of pipe
 		y2<= newLength+gap;  -- determiens y2 pos
 		--pipe_x_pos <= CONV_STD_LOGIC_VECTOR(600,11);
-		pipe1_y_pos <= CONV_STD_LOGIC_VECTOR(0,10);
-		pipe2_y_pos <= y2;
+		pipe1top_y_pos <= CONV_STD_LOGIC_VECTOR(0,10);
+		pipe1bottom_y_pos <= y2;
 
 								-- and here is adding 0 to ball_x_pos making it unsigned
-		temp_pipe1_on <= '1' when ( ('0' & pipe_x_pos <= '0' & pixel_column + sizex) and ('0' & pixel_column <= '0' & pipe_x_pos + sizex) 	-- x_pos - size <= pixel_column <= x_pos + size
-							and ('0' & pixel_row >= pipe1_y_pos) and ('0' & pixel_row <= sizey) and (mode = "001" or mode = "010") )  else	-- y_pos - size <= pixel_row <= y_pos + size
+		temp_pipetop1_on <= '1' when ( ('0' & pipe1_x_pos <= '0' & pixel_column + sizex) and ('0' & pixel_column <= '0' & pipe1_x_pos + sizex) 	-- x_pos - size <= pixel_column <= x_pos + size
+							and ('0' & pixel_row >= pipe1top_y_pos) and ('0' & pixel_row <= sizey) and (mode = "001" or mode = "010") )  else	-- y_pos - size <= pixel_row <= y_pos + size
 					'0';
-		temp_pipe2_on <= '1' when ( ('0' & pipe_x_pos <= '0' & pixel_column + sizex) and ('0' & pixel_column <= '0' & pipe_x_pos + sizex) 	-- x_pos - size <= pixel_column <= x_pos + size
-							and ('0' & pipe2_y_pos <= pixel_row) and ('0' & pixel_row <= 479) and (mode = "001" or mode = "010") )  else	-- y_pos - size <= pixel_row <= y_pos + size
+		temp_pipebottom1_on <= '1' when ( ('0' & pipe1_x_pos <= '0' & pixel_column + sizex) and ('0' & pixel_column <= '0' & pipe1_x_pos + sizex) 	-- x_pos - size <= pixel_column <= x_pos + size
+							and ('0' & pipe1bottom_y_pos <= pixel_row) and ('0' & pixel_row <= 479) and (mode = "001" or mode = "010") )  else	-- y_pos - size <= pixel_row <= y_pos + size
 					'0';
 --checks if pixel is in the ball boundary 
-pipe_on <= temp_pipe1_on or temp_pipe2_on;
+pipe_on <= temp_pipetop1_on or temp_pipetop2_on;
 
 Spawn_Move_pipe: process (vert_sync,newLength,y2)  	
 variable holdLength:std_logic_vector(9 downto 0);
@@ -58,7 +63,7 @@ begin
 -- change x1 and x2 accordingly and set it
 --then move
 	if (rising_edge(vert_sync)) then
-		if(pipe_x_pos <= CONV_STD_LOGIC_VECTOR(0,11) or mode = "000") then
+		if(pipe1_x_pos <= CONV_STD_LOGIC_VECTOR(0,11) or mode = "000") then
 
 			holdLength := "00"& randomNumber;
 			
@@ -69,11 +74,11 @@ begin
 			
 			newLength<=holdLength;
 			
-			pipe_x_pos <= CONV_STD_LOGIC_VECTOR(638,11);	
+			pipe1_x_pos <= CONV_STD_LOGIC_VECTOR(638,11);	
 		
-		elsif (pipe_x_pos > CONV_STD_LOGIC_VECTOR(0,11)and pipe_x_pos < CONV_STD_LOGIC_VECTOR(639,11))  then
-			pipe_x_motion <= -CONV_STD_LOGIC_VECTOR(12,11); -- move left to right
-			pipe_x_pos <= pipe_x_pos + pipe_x_motion;
+		elsif (pipe1_x_pos > CONV_STD_LOGIC_VECTOR(0,11)and pipe1_x_pos < CONV_STD_LOGIC_VECTOR(639,11))  then
+			pipe1_x_motion <= -CONV_STD_LOGIC_VECTOR(12,11); -- move left to right
+			pipe1_x_pos <= pipe1_x_pos + pipe1_x_motion;
 		
 		end if;
 		resetNumGen<='1';
