f9a77a643856634f21bb7af4e2a0adcd5c2e693b
rnat697 <rnat697@aucklanduni.ac.nz>
2022-05-28 19:18:19 +1200

    added lives as input to fsm

--- miniproject files/MiniprojectResources/statemachinet.vhd
+++ miniproject files/MiniprojectResources/statemachinet.vhd
@@ -4,9 +4,7 @@ entity fsm is
     Port (reset : in  STD_LOGIC; -- connected to pb2
 			 pb0_in: in STD_LOGIC;
 			 pb1_in: in STD_LOGIC;
-			 input : in  STD_LOGIC;
-			 x: 	in std_logic;
-			 parity: out STD_LOGIC; -- score?
+			 lives: 	in integer;
 			 mode_out : out  STD_LOGIC_VECTOR (2 downto 0);
 			 clk : in  STD_LOGIC);
 end fsm;
@@ -20,8 +18,7 @@ type state_type is (S0,S1,S2,S3,S4,S5);
 --	S1 trainingmode
 --	S2 actualmode
 -- S3 gameover (u died screen)
--- S4 game finished (in training mode after 20 pipes or smth)
--- S5 (maybe different background harder difficulyt
+-- S4 (maybe different background harder difficulyt
 signal state,next_state: state_type;
 signal pb1,pb2: std_logic;
 
@@ -39,7 +36,7 @@ begin
 
 end process;
 
-OUTPUT_DECODE : process(state,pb0_in,pb1_in,x)
+OUTPUT_DECODE : process(state,pb0_in,pb1_in,lives)
 begin
 		case(state) is
 			--Mainmenu
@@ -51,21 +48,17 @@ begin
 				mode_out<="001";
 			when S2 => -- S2 in actual game mode
 				mode_out <="010";
+			when S3 => -- Game over screen
+				mode_out <= "011";
 			when others =>
 				mode_out <= "000";
-			--pass
-			--when S1 =>
-			--	if(x='1') then
-			--		parity <= '1';
-			--	end if;
-			--S2 does not have a finish, endless
 		end case;
 end process OUTPUT_DECODE;		
-NEXT_STATE_DECODE: process(state,pb0_in,pb1_in,x)
+NEXT_STATE_DECODE: process(state,pb0_in,pb1_in,lives)
 begin
 
 	case (state) is
-		when S0 =>
+		when S0 => -- MAIN MENU ---
 			if(pb0_in='0') then
 				next_state<=S1;	--trainingmode
 			elsif(pb1_in='0') then
@@ -73,21 +66,30 @@ begin
 			else -- Stay on main menu if nothing happens
 				next_state<=S0;
 			end if;	
-		when S1 =>
-			if(x ='1') then
-				next_state<=S3;
+			
+		when S1 =>	--- TRAIN MODE ---
+			if(lives = 0) then
+				next_state<=S3; -- Go to game over screen
 			elsif(reset='0') then
 				next_state<=S0;
 			else	-- Stay on training mode if no inputs
 				next_state<=S1;
-			end if;
-		when S2 =>
-			if(x='1') then
-				next_state<=S3;
+			end if; 
+			
+		when S2 =>	--- GAME MODE ---
+			if(lives= 0) then
+				next_state<=S3; -- go to game over screen
 			elsif(reset='0') then
 				next_state<=S0;
 			else -- stay on game mode if no inputs
 				next_state<=S2;
+			end if;
+			
+		when S3 => 	--- GAME OVER ---
+			if (reset = '0') then
+				next_state <= S0; -- go back to main menu
+			else
+				next_state <= S3; -- stay on game over screen if no inputs
 			end if;
 			
 		when others =>
