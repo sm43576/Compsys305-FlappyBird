b9347c1623b59edbd8d95c1e799bc4e59ca3d02d
sm43576 <79734318+sm43576@users.noreply.github.com>
2022-05-29 19:59:32 +1200

    Revert "revertrevert"
    This reverts commit b857b53af1dd4ea5724d9637c5ec139e60fab7dc.

--- miniproject files/MiniprojectResources/textToDisplay.vhd
+++ miniproject files/MiniprojectResources/textToDisplay.vhd
@@ -145,7 +145,6 @@ begin
 					
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 				if(32<p_row and p_row<48) and (96<p_col and p_col<112) then
 						textOn <= '1';
 						charvalue <= sigNumScore(lives);		-- display life
@@ -155,8 +154,6 @@ begin
 					textOn<='0';
 				end if;
 				
-=======
->>>>>>> parent of 56459b7 (remove commit if not fixed,( approach display lvies))
 =======
 >>>>>>> parent of 56459b7 (remove commit if not fixed,( approach display lvies))
 =======
@@ -198,12 +195,9 @@ begin
 		
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 					for i in 0 to 5 loop
 						if(32<p_row and p_row<48) and (((i-1)*16)+16<p_col and p_col<16+(i*16))	 then
 =======
-=======
->>>>>>> parent of 56459b7 (remove commit if not fixed,( approach display lvies))
 =======
 >>>>>>> parent of 56459b7 (remove commit if not fixed,( approach display lvies))
 					for i in 0 to 6 loop
@@ -223,7 +217,6 @@ begin
 					
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 					
 					if(32<p_row and p_row<48) and (96<p_col and p_col<112)	then
 						textOn <= '1';
@@ -233,8 +226,6 @@ begin
 					else
 						textOn <= '0';
 					end if;
-=======
->>>>>>> parent of 56459b7 (remove commit if not fixed,( approach display lvies))
 =======
 >>>>>>> parent of 56459b7 (remove commit if not fixed,( approach display lvies))
 =======
